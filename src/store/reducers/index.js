958d922185a8e5f0ac12147ff70391f0fbe4d933
So Rishabh <sorishabh@MacBook-Pro.local>
2023-10-04 16:09:51 +0530

    added redux practice1

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import { productReducer, selectedProductsReducer } from "./productReducer";
 import { cartReducer } from "./cartReducer"; // Create this file next
-
 const reducers = combineReducers({
   allProducts: productReducer,
   product: selectedProductsReducer,
